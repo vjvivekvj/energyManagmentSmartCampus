030ab0d1abd670c4f075dd91af0c04d35992d31b
vjvivekvj <vivekj.joshi9@gmail.com>
2021-08-31 10:37:36 +0530

    Create truffle project

---
+++ contracts/energy_management.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.5.0 ;
+
+contract energy_management{
+    
+    uint public current_available_energy = 100 ;
+    uint public constant min_price = 10;
+    
+    mapping(address => uint) wallet ;
+    
+    function current_price() public pure returns(uint){
+        return min_price;
+    }
+    
+    function display_wallet(address prosumer) external view returns(uint){
+        return wallet[prosumer] ;
+    }
+    
+    function sell_energy(address prosumer, uint amount) external returns(uint){
+        uint sold_for = amount * current_price() ;
+        wallet[prosumer] += sold_for ;
+        current_available_energy += amount ;
+        return sold_for ;
+    }
+    
+    modifier can_buy_energy(address prosumer, uint amount){
+        require(wallet[prosumer] >= current_price() * amount && current_available_energy>amount) ;
+        _;
+    }
+    
+    function buy_energy(address prosumer, uint amount) external
+    can_buy_energy(prosumer, amount){
+        wallet[prosumer] -= amount*current_price();
+        current_available_energy -= amount ;
+    }
+}
